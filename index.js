f7fc8a46c648913eb9f0c00148f94cbc74a5bc13
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2024-04-13 09:57:22 +0100

    completed JavaScript and created test MVP

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
-const fs = require('fs');
-const objectsToCsv =require ('objects-to-csv');
+const fs = require("fs");
+const ObjectsToCsv = require("objects-to-csv");
 
 async function saveHackerNewsArticles() {
   // launch browser
@@ -12,21 +11,33 @@ async function saveHackerNewsArticles() {
   // go to Hacker News
   await page.goto("https://news.ycombinator.com");
 
-  //Find the top 10 articles
-const articles = await page.$$eval('.storylink',(links)=>{
-  const articlesData =[];
-  for (let i =0; i<10; i++){
-    articlesData.push({
-      title: links[i].innerText,
-      url:links[i].href
-    });
-  }
-  return articlesData
-});
+  // Find the top 10 articles
+  const articles = await page.$$eval(".storylink", (links) => {
+    const articlesData = [];
+    for (let i = 0; i < 10 && i < links.length; i++) {
+      if (links[i]) {
+        articlesData.push({
+          title: links[i].innerText,
+          url: links[i].href,
+        });
+      }
+    }
+    return articlesData;
+  });
 
+  // Save data to CSV file
+  const csv = new ObjectsToCsv(articles);
+  await csv.toDisk("./hacker_news_articles.csv");
 
+  // Close the browser after a delay of 10 seconds
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  await browser.close();
 }
 
 (async () => {
-  await saveHackerNewsArticles();
+  try {
+    await saveHackerNewsArticles();
+  } catch (error) {
+    console.error("An error occured:", error);
+  }
 })();
